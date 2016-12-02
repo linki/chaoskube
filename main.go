16b7950cce3129fdc9ea501a1f133074e07f8d65
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2016-12-02 17:59:10 +0100

    feat: support finding the cluster from the environment

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 
@@ -13,40 +14,63 @@ import (
 )
 
 var (
-	interval time.Duration
+	interval  time.Duration
+	inCluster bool
 )
 
 func init() {
 	kingpin.Flag("interval", "Interval between Pod terminations").Short('i').Default("10m").DurationVar(&interval)
+	kingpin.Flag("in-cluster", "If true, finds the Kubernetes cluster from the environment").Short('c').BoolVar(&inCluster)
 }
 
 func main() {
 	kingpin.Parse()
 
-	config := &rest.Config{
-		Host: "http://127.0.0.1:8001",
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
+	client, err := newClient()
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
 
 	for {
-		pods, err := clientset.Core().Pods("").List(v1.ListOptions{})
+		pods, err := client.Core().Pods(v1.NamespaceAll).List(v1.ListOptions{})
 		if err != nil {
-			panic(err.Error())
+			log.Fatal(err)
 		}
 
 		victim := pods.Items[rand.Intn(len(pods.Items))]
 
 		fmt.Printf("Killing pod %s/%s\n", victim.Namespace, victim.Name)
 
-		err = clientset.Core().Pods(victim.Namespace).Delete(victim.Name, nil)
+		err = client.Core().Pods(victim.Namespace).Delete(victim.Name, nil)
 		if err != nil {
-			panic(err.Error())
+			log.Fatal(err)
 		}
 
 		time.Sleep(interval)
 	}
+}
+
+func newClient() (*kubernetes.Clientset, error) {
+	var (
+		config *rest.Config
+		err    error
+	)
+
+	if inCluster {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		config = &rest.Config{
+			Host: "http://127.0.0.1:8001",
+		}
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }

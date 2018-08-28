7e9655ccf2fbb07ff37263ea88da357f57886bd2
Shai Katz <shai@soluto.com>
2018-08-28 15:14:13 +0300

    add missing file

---
+++ metrics/metrics.go
@@ -0,0 +1,29 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	// PodsDeletedCounter is the pods deleted counter
+	PodsDeletedCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "pods_deleted",
+		Help: "The total number of pods deleted",
+	})
+	// RunCounter is the run function executions counter
+	RunCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "run_counts",
+		Help: "The total number of pod termination logic runs",
+	})
+	// ErrorCounter is the run function executions counter
+	ErrorCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "termination_errors",
+		Help: "The total number of errors on terminate victim operation",
+	})
+	// errorCounter is the run function executions counter
+	TerminationHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "termination_time_milliseconds",
+		Help: "The time took single pod termination to finish",
+	})
+)
